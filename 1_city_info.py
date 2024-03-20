aa064288d2d536837c4a87830c20f5e98fad0dcc
Your Name <you@example.com>
2024-03-20 13:16:55 +0200

    debug2

--- 1_city_info.py
+++ 1_city_info.py
@@ -10,25 +10,33 @@ import urllib.request
 import json
 
 def get_city_information(city):
-    link = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=3&language=en&format=json"
+    link = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=3&language=lv&format=json"
     
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(link) as response:
         data = response.read().decode('utf-8')
 
     return json.loads(data)
 
 def display_city_information(city_info):
-    if city_info
+    if city_info:
         print("City Information:")
         for city_data in city_info['results']:
             print(f"Name: {city_data['name']}")
-            print(f"Country: {city_data['county']}")
-            print(f"Latitude: {city_data['lat']}")
-            print(f"Longitude: {city_data['lon']}")
+            print(f"Country: {city_data['country']}")
+            print(f"Latitude: {city_data['latitude']}")
+            print(f"Longitude: {city_data['longitude']}")
+            print(f"Population: {city_data['population']}")
+            print(f"Time zone: {city_data['timezone']}")
+            print(f"Country_id: {city_data['country_id']}")
             print("-----------------------")
     else:
         print("No city information available.")
 
 city_name = input("Enter city name: ")
+if len(city_name) < 2:
+    print("Error")
+    exit()
+
+
 city_information = get_city_information(city_name)
 display_city_information(city_information)
--- 2_location_weather.py
+++ 2_location_weather.py
@@ -4,3 +4,16 @@
 # izmantojot sekojošu datu linku 
 # https://api.open-meteo.com/v1/forecast?latitude=56.8&longitude=24.2&hourly=temperature_2m,precipitation&forecast_days=1
 # 
+
+
+import urllib.request
+import json
+
+def get_city_information():
+    link = f"https://api.open-meteo.com/v1/forecast?latitude=56.8&longitude=24.2&hourly=temperature_2m,precipitation&forecast_days=1"
+    
+    with urllib.request.urlopen(link) as response:
+        data = response.read().decode('utf-8')
+
+    return json.loads(data)
+
